7723b763741e3c0c345e4a4f9ce2fc02423488f3
Dan Damian <damian-dan@users.noreply.github.com>
2015-12-24 22:34:25 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+apt-get install build-essential nano
+
+apt-get install libfcgi-dev libfcgi0ldbl libjpeg62-turbo-dbg libmcrypt-dev libssl-dev libc-client2007e libc-client2007e-dev libxml2-dev libbz2-dev libcurl4-openssl-dev libjpeg-dev libpng12-dev libfreetype6-dev libkrb5-dev libpq-dev libxml2-dev libxslt1-dev
